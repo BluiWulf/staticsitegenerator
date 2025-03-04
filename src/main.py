6137714d1b6927c3d5336667a7bff012973a1ad6
BluiWulf <bluiwulf@gmail.com>
2025-03-04 12:37:11 -0500

    Adding leafnode and testing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode
+from leafnode import LeafNode
 
 def main():
-    html_node = HTMLNode("h1", "This is a header", [HTMLNode("p", "This is a paragraph"), HTMLNode("p", "This is a second paragraph")], {"href": "https://www.google.com", "target": "_blank",})
-    print(html_node.__repr__())
+    node_children = [
+        HTMLNode("p", "This is a paragraph"),
+        HTMLNode("p", "This is a second paragraph")
+    ]
+    node_props = {
+        "href": "https://lycanwaredevelopments.org",
+        "align": "center",
+        "bgcolor": "black",
+        "id": "dummy",
+    }
+
+    text_node = TextNode("This is some anchor text", TextType.LINK_TEXT, 'https://www.boot.dev')
+    html_node = HTMLNode("h1", "This is a header", node_children, node_props)
+    leaf_node = LeafNode("h2", "This is a 2nd header", node_props)
+
+    print(f"{text_node.__repr__()}\n")
+    print(f"{html_node.__repr__()}\n")
+    print(f"{leaf_node.to_html()}\n")
 
 if __name__ == "__main__":
     main()
---
+++ src/test_leafnode.py
@@ -0,0 +1,38 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf0(self):
+        expected = "<p>Hello, world!</p>"
+
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), expected)
+
+    def test_leaf1(self):
+        expected = "<h1>This is the header</h1>"
+
+        node = LeafNode("h1", "This is the header")        
+        self.assertEqual(node.to_html(), expected)
+
+    def test_leaf2(self):
+        expected = '<h2 href="https://lycanwaredevelopments.org">This is the second header</h2>'
+
+        node = LeafNode("h2", "This is the second header", {"href": "https://lycanwaredevelopments.org",})
+        self.assertEqual(node.to_html(), expected)
+
+    def test_leaf3(self):
+        node_props = {
+            "href": "https://lycanwaredevelopments.org",
+            "align": "center",
+            "bgcolor": "black",
+            "id": "dummy",
+        }
+        expected = '<p href="https://lycanwaredevelopments.org" align="center" bgcolor="black" id="dummy">This is the paragraph</p>'
+
+        node = LeafNode("p", "This is the paragraph", node_props)
+        self.assertEqual(node.to_html(), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()

377f3b13bff8f289004cbb2ebf5d10a668e67f93
BluiWulf <bluiwulf@gmail.com>
2025-03-06 02:54:26 -0500

    updating parser to match solution files

--- src/parser.py
+++ src/parser.py
@@ -6,14 +6,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type != TextType.NORMAL_TEXT:
             new_nodes.append(node)
-        else:
-            splits = node.text.split(delimiter)
-            if len(splits) == 1:
-                raise Exception("Node contains invalid Markdown syntax")
-            new_nodes.extend([
-                TextNode(splits[0], TextType.NORMAL_TEXT),
-                TextNode(splits[1], text_type),
-                TextNode(splits[2], TextType.NORMAL_TEXT)
-            ])
+            continue
+        splits = node.text.split(delimiter)
+        sections = []
+        if len(splits) % 2 == 0:
+            raise Exception("Node contains invalid Markdown syntax")
+        for i in range(len(splits)):
+            if splits[i] == "":
+                continue
+            if i % 2 == 0:
+                sections.append(TextNode(splits[i], TextType.NORMAL_TEXT))
+            else:
+                sections.append(TextNode(splits[i], text_type))
+        new_nodes.extend(sections)
 
     return new_nodes

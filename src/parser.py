6827b949f28063580ac53d0cb6cc548fac2a6797
BluiWulf <bluiwulf@gmail.com>
2025-03-07 16:04:26 -0500

    Adding text_to_textnodes definition

--- src/parser.py
+++ src/parser.py
@@ -72,6 +72,11 @@ def split_nodes_link(old_nodes):
     
     return new_nodes
 
+def text_to_textnodes(text):
+    pass
+
+# Helper Functions
+
 def extract_markdown_images(text):
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 

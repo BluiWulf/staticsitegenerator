a7c3391f835b4d9b3bff666405a1f7794c11b234
BluiWulf <bluiwulf@gmail.com>
2025-04-05 15:58:23 -0400

    Added function to copy contents of static directory

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode
-from leafnode import LeafNode
-from parentnode import ParentNode
-from inline_parser import *
-from block_parser import *
-from converter import *
-
-import re
+import os
+import shutil
 
 def main():
-    # This section is being used for very quick unit testing before creating addition tests
-    print()
+    copy_static()
+
+def copy_static(base_path = ""):
+    contents = []
+    main_src = os.path.join("static", base_path)
+    main_dest = os.path.join("public", base_path)
+
+    if os.path.exists(main_dest):
+        shutil.rmtree(main_dest)
+    os.mkdir(main_dest)
+
+    contents.extend(os.listdir(main_src))
+    for item in contents:
+        src = os.path.join(main_src, item)
+        dest = os.path.join(main_dest, item)
+        print(f" * {src} -> {dest}")
+        if os.path.isfile(src):
+            shutil.copy(src, dest)
+            continue
+        if not os.path.exists(dest):
+            copy_static(item)
 
 if __name__ == "__main__":
     main()
